6c671ffadeb859dcc69bc6f6d01ea7c23622900c
MehdiM62 <m_mehdi_m62@yahoo.com>
2018-09-26 15:43:48 +0800

    TestCase eg1

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
   courses, // the way that we call it within state( ES6 short hand property name)
-  authors
+  authors,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;
